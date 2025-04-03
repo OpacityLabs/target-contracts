67de0f64e222f9386426ef62122b66c05e9ebb82
rubydusa <rubydusa@protonmail.com>
2025-04-03 20:42:33 +0300

    chore: rename RegistryCoordinatorMock -> RegistryCoordinatorMimic

--- src/RegistryCoordinatorMimic.sol
+++ src/RegistryCoordinatorMimic.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.12;
 
-contract RegistryCoordinatorMock {
+contract RegistryCoordinatorMimic {
 
     /**
      * @notice The total number of quorums that have been created.
