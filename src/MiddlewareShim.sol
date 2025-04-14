ea11c7773c945fdae09242d35444f98281ac74e1
rubydusa <rubydusa@protonmail.com>
2025-04-15 00:27:53 +0300

    chore: set contract licenses to UNLICENSED for now

--- src/MiddlewareShim.sol
+++ src/MiddlewareShim.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.12;
 
 import {BitmapUtils} from "@eigenlayer-middleware/libraries/BitmapUtils.sol";
