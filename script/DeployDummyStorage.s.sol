13bd98a3875ce148c1365321aae11c205e36a1bc
rubydusa <rubydusa@protonmail.com>
2025-04-14 19:33:25 +0300

    feat: simple storage proof verification on-chain

---
+++ script/DeployDummyStorage.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import {Script, console} from "forge-std/Script.sol";
+
+contract DeployDummyStorage is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        Counter counter = new Counter();
+        counter.increment();
+        console.log("Counter deployed at", address(counter));
+
+        vm.stopBroadcast();
+    }
+}
+
+contract Counter {
+    uint256 public count;
+
+    function increment() external {
+        count++;
+    }
+
+    function decrement() external {
+        count--;
+    }
+}
