8e633e2a1f16fecee46c8be0984e183499e90cd0
rubydusa <rubydusa@protonmail.com>
2025-04-16 12:52:17 +0300

    refactor: remove unecessary block.number - 1
    chore: forge fmt
    
    chore: clean DeployEnvironement

--- script/DeployEnvironment.s.sol
+++ script/DeployEnvironment.s.sol
@@ -17,17 +17,15 @@ contract DeployEnvironment is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        MiddlewareShim middlewareShim = new MiddlewareShim(ISlashingRegistryCoordinator(OPACITY_REGISTRY_COORDINATOR_ADDRESS_HOLESKY));
+        MiddlewareShim middlewareShim =
+            new MiddlewareShim(ISlashingRegistryCoordinator(OPACITY_REGISTRY_COORDINATOR_ADDRESS_HOLESKY));
         console.log("MiddlewareShim deployed at:", address(middlewareShim));
 
-        RegistryCoordinatorMimic registryCoordinatorMimic = new RegistryCoordinatorMimic(SP1Helios(address(0)), address(middlewareShim));
+        RegistryCoordinatorMimic registryCoordinatorMimic =
+            new RegistryCoordinatorMimic(SP1Helios(address(0)), address(middlewareShim));
         console.log("RegistryCoordinatorMimic deployed at:", address(registryCoordinatorMimic));
 
-        MiddlewareShim.MiddlewareData memory middlewareData = middlewareShim.updateMiddlewareDataHash();
-        console.log("MiddlewareData update block number:", middlewareData.blockNumber);
-        // // I'm only storing the block number because serializing the getMiddlewareData() output is a pain
-        // string memory json = string.concat("{\"blockNumber\": ", Strings.toString(middlewareData.blockNumber), "}");
-        // vm.writeJson(json, "middlewareData.json");
+        middlewareShim.updateMiddlewareDataHash();
 
         vm.stopBroadcast();
     }
