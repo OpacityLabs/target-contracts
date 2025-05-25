ca8ed127594063965211c77bc4e8311460cb9417
rubydusa <rubydusa@protonmail.com>
2025-05-25 22:42:46 +0300

    chore: rename `get-sp1-block` -> `get-sp1-slot`

--- test/e2e/scripts/run-testnet.sh
+++ test/e2e/scripts/run-testnet.sh
@@ -17,12 +17,12 @@ echo "Deploying bridge contracts on L1 and L2..."
 echo "Updating middleware shim with latest data hash..."
 ./update-shim.sh || handle_error $?
 
-echo "Getting latest SP1Helios block number..."
-BLOCK_NUMBER=$(./get-sp1-block.sh | tail -n1) || handle_error $?
-echo "Block number: $BLOCK_NUMBER"
+echo "Getting latest SP1Helios slot number..."
+SLOT_NUMBER=$(./get-sp1-slot.sh | tail -n1) || handle_error $?
+echo "Slot number: $SLOT_NUMBER"
 
 echo "Generating proof..."
-./generate-proof.sh $BLOCK_NUMBER || handle_error $?
+./generate-proof.sh $SLOT_NUMBER || handle_error $?
 
 echo "Updating middleware shim with latest data hash..."
 ./update-mimic.sh || handle_error $?
